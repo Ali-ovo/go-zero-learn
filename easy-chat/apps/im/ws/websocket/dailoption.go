aff4dd5867a7ac696f3b9bf666eb950baab16ce3
Ali <ali_ovo@qq.com>
2024-12-18 17:44:58 +0800

    feat: update configuration for Redis and Kafka; add WebSocket client implementation and connection options

---
+++ easy-chat/apps/im/ws/websocket/dailoption.go
@@ -0,0 +1,35 @@
+package websocket
+
+import "net/http"
+
+type DailOptions func(option *dailOption)
+
+type dailOption struct {
+	pattern string
+	header  http.Header
+}
+
+func newDailOptions(opts ...DailOptions) dailOption {
+	o := dailOption{
+		pattern: "/ws",
+		header:  nil,
+	}
+
+	for _, opt := range opts {
+		opt(&o)
+	}
+
+	return o
+}
+
+func WithClientPattern(pattern string) DailOptions {
+	return func(option *dailOption) {
+		option.pattern = pattern
+	}
+}
+
+func WithClientHeader(header http.Header) DailOptions {
+	return func(option *dailOption) {
+		option.header = header
+	}
+}
--- easy-chat/apps/user/rpc/internal/svc/servicecontext.go
+++ easy-chat/apps/user/rpc/internal/svc/servicecontext.go
@@ -3,12 +3,17 @@ package svc
 import (
 	"easy-chat/apps/user/models"
 	"easy-chat/apps/user/rpc/internal/config"
+	"easy-chat/pkg/constants"
+	"easy-chat/pkg/ctxdata"
+	"time"
 
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	*redis.Redis
 
 	models.UsersModel
 }
@@ -18,6 +23,18 @@ func NewServiceContext(c config.Config) *ServiceContext {
 
 	return &ServiceContext{
 		Config:     c,
+		Redis:      redis.MustNewRedis(c.Redisx),
 		UsersModel: models.NewUsersModel(sqlConn, c.Cache),
 	}
+}
+
+func (svc *ServiceContext) SetRootToken() error {
+	// 生成jwt
+	systemToken, err := ctxdata.GetJwtToken(svc.Config.Jwt.AccessSecret, time.Now().Unix(), 999999999, constants.SYSTEM_ROOT_UID)
+	if err != nil {
+		return err
+	}
+	// 写入到redis
+
+	return svc.Redis.Set(constants.REDIS_SYSTEM_ROOT_TOKEN, systemToken)
 }
