f93e5b9ef8ba68bd2bf93a1eb25224d74c2abeaf
ali <ali_ovo@qq.com>
2024-12-25 20:20:41 +0800

    feat: update configuration for IM services; change ports and add new RPC and API test scripts

--- easy-chat/deploy/script/im-api-test.sh
+++ easy-chat/deploy/script/im-api-test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-reso_addr='crpi-hisvoxurvww9uula.cn-shanghai.personal.cr.aliyuncs.com/ali-easy-chat/social-rpc-dev'
+reso_addr='crpi-hisvoxurvww9uula.cn-shanghai.personal.cr.aliyuncs.com/ali-easy-chat/im-api-dev'
 tag='latest'
 
-pod_ip="172.20.209.226"
-
-container_name="easy-chat-social-rpc-test"
+container_name="easy-chat-im-api-test"
 
 docker stop ${container_name}
 
@@ -17,4 +15,4 @@ docker pull ${reso_addr}:${tag}
 
 # 如果需要指定配置文件的
 # docker run -p 10001:8080 --network imooc_easy-im -v /easy-im/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-docker run -p 10001:10001 -e POD_IP=${pod_ip}  --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p 8882:8882  --name=${container_name} -d ${reso_addr}:${tag}
